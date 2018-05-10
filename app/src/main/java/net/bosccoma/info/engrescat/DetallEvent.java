dac9cdbec64997a2bd8e60848086528e636bbb69
Jordi Coma Cano <jordicom2@yahoo.es>
2018-05-10 17:22:05 +0200

    Detall event 3 botons Boto share funcionant

--- app/src/main/java/net/bosccoma/info/engrescat/DetallEvent.java
+++ app/src/main/java/net/bosccoma/info/engrescat/DetallEvent.java
@@ -1,17 +1,45 @@
 package net.bosccoma.info.engrescat;
 
+import android.content.Intent;
 import android.media.Image;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by JordiC on 27/04/2018.
  */
 
-public class DetallEvent {
+public class DetallEvent extends AppCompatActivity {
     private String Name, ImageURL;
+    FloatingActionButton bt_share;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+        bt_share=(FloatingActionButton) findViewById(R.id.btn_share);
+        bt_share.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = "Cos de l'event";
+                String shareSub = "Titol de l'event";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT,shareBody);
+                myIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+                startActivity(Intent.createChooser(myIntent,"Compartir amb:"));  //Titol de l'activity
+            }
+        });
+    }
     public DetallEvent(String name, String imageURL){
         Name = name;
         ImageURL = imageURL;
+    }
+
+    public DetallEvent() {
     }
 
     public String getName() {
@@ -29,4 +57,6 @@ public class DetallEvent {
     public void setImageURL(String imageURL) {
         ImageURL = imageURL;
     }
+
+
 }
